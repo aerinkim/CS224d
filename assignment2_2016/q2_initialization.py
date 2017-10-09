43842d8ed85c6edcf2fe3f7bf17d2b3eb78ccdf3
byorxyz <ohmyheroine@hotmail.com>
2017-10-08 18:34:51 -0700

    initialization code is done

--- assignment2_2016/q2_initialization.py
+++ assignment2_2016/q2_initialization.py
@@ -4,27 +4,27 @@ import tensorflow as tf
 def xavier_weight_init():
   """
   Returns function that creates random tensor. 
-
   The specified function will take in a shape (tuple or 1-d array) and must
   return a random tensor of the specified shape and must be drawn from the
   Xavier initialization distribution.
-
   Hint: You might find tf.random_uniform useful.
   """
   def _xavier_initializer(shape, **kwargs):
     """Defines an initializer for the Xavier distribution.
-
     This function will be used as a variable scope initializer.
-
     https://www.tensorflow.org/versions/r0.7/how_tos/variable_scope/index.html#initializers-in-variable-scope
-
     Args:
       shape: Tuple or 1-d array that species dimensions of requested tensor.
     Returns:
       out: tf.Tensor of specified shape sampled from Xavier distribution.
     """
     ### YOUR CODE HERE
-    raise NotImplementedError
+    m = shape[0]
+    if len(shape)>1:
+      n = shape[1]
+    else:
+      n = 0
+    out = tf.random_uniform(shape, minval = -np.sqrt(6)/np.sqrt(m+n), maxval = np.sqrt(6)/np.sqrt(m+n))
     ### END YOUR CODE
     return out
   # Returns defined initializer function.
@@ -54,7 +54,7 @@ def test_initialization():
   """
   print "Running your tests..."
   ### YOUR CODE HERE
-  raise NotImplementedError
+  #raise NotImplementedError
   ### END YOUR CODE  
 
 if __name__ == "__main__":
